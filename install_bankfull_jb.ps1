6ce5edbcd62c24da45e11e63988c5b94177ae231
Juliette Brochier <juliette.BROCHIER@strasbourg.eu>
2024-07-18 11:49:51 +0200

    Sauvegarde avec nouveau scriptd'installation

--- install_bankfull_jb.ps1
+++ install_bankfull_jb.ps1
@@ -1,3 +1,4 @@
-$env:PYTHONHOME = 'C:\Program Files\QGIS 3.20.3\apps\Python39'
-C:\Program Files\QGIS 3.20.3\apps\Python39\python3 -m pip install --upgrade -r .\bankfull_module_jb\requirements_ems.txt --user
+$env:PYTHONHOME="C:\Program Files\QGIS 3.20.3\apps\Python39"
+& 'C:\Program Files\QGIS 3.20.3\apps\Python39\Scripts\pip' install --upgrade -r .\bankfull_module_jb\requirements_ems.txt --user
 Move-Item -Path .\bankfull_module_jb\ -Destination $env:APPDATA\QGIS\QGIS3\profiles\default\python\plugins\ -Force
+PAUSE
