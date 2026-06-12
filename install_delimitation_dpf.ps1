f506d90e6fdac18d5ccf8e3ac16750f1a9570192
WEHRLE Sébastien (Eurométropole) <sebastien.wehrle@strasbourg.eu>
2026-06-12 08:01:06 +0000

    Modification du répertoire local des plugins

--- install_delimitation_dpf.ps1
+++ install_delimitation_dpf.ps1
@@ -1,4 +1,4 @@
 $env:PYTHONHOME="C:\Program Files\QGIS 3.28.4\apps\Python39"
 & 'C:\Program Files\QGIS 3.28.4\apps\Python39\Scripts\pip' install --upgrade -r .\delimitation_dpf\requirements_ems.txt --user
-Move-Item -Path .\delimitation_dpf\ -Destination $env:APPDATA\QGIS\QGIS3\profiles\default\python\plugins\ -Force
+Move-Item -Path .\delimitation_dpf\ -Destination $env:APPDATA\QGIS\QGIS3284-PROD\profiles\default\python\plugins\ -Force
 PAUSE
